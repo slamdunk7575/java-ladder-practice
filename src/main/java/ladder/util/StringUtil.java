90a8ac63e308d13a84823b938d2f35a3990abcc5
slamdunk7575 <slamdunk7575@gmail.com>
2021-02-22 01:11:41 +0900

    (feat) StringUtil 클래스 추가

---
+++ src/main/java/ladder/util/StringUtil.java
@@ -0,0 +1,29 @@
+package ladder.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringUtil {
+    private static final String COMMA = ",";
+    private static final String EMPTY = "";
+    private static final String BLANK = " ";
+
+    private StringUtil() {
+    }
+
+    public static boolean isEmpty(String text) {
+        return text == null || EMPTY.equals(text);
+    }
+
+    public static List<String> convertList(String text) {
+        if(isEmpty(text)) {
+            throw new IllegalArgumentException("입력 값이 없습니다.");
+        }
+        text = removeBlank(text.trim());
+        return Arrays.asList(text.split(COMMA));
+    }
+
+    private static String removeBlank(String input) {
+        return input.replace(BLANK, EMPTY);
+    }
+}
