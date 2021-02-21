6985c1710e0800fb6792c225ff3f82d739bd9b22
slamdunk7575 <slamdunk7575@gmail.com>
2021-02-21 23:47:51 +0900

    (feat) Ladder 추가

---
+++ src/main/java/ladder/domain/Ladder.java
@@ -0,0 +1,46 @@
+package ladder.domain;
+
+import ladder.rule.DrawRule;
+import lombok.Builder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ladder {
+
+    private final List<LadderLine> ladderLines;
+    private final int columnCount;
+    private final DrawRule drawRule;
+
+    @Builder
+    public Ladder(int rowCount, int columnCount, DrawRule drawRule) {
+        this.ladderLines = initLadder(rowCount, columnCount, drawRule);
+        this.columnCount = columnCount;
+        this.drawRule = drawRule;
+    }
+
+    private List<LadderLine> initLadder(int rowCount, int columnCount, DrawRule drawRule) {
+        return Stream.generate(() -> LadderLine.init(columnCount, drawRule))
+                .limit(rowCount)
+                .collect(Collectors.toList());
+    }
+
+    public int searchFinalPosition(int startPoint) {
+        int currentPosition = startPoint;
+        for (LadderLine line :  ladderLines) {
+            currentPosition = line.move(currentPosition);
+        }
+        return currentPosition;
+    }
+
+    public List<LadderLine> getLadderLines() {
+        return Collections.unmodifiableList(ladderLines);
+    }
+
+    public int getColumnCount() {
+        return this.columnCount;
+    }
+
+}
