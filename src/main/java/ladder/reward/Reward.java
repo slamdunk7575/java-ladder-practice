ddf64a4c6ed2b8d9bcbaee13084f30ae1014ca40
slamdunk7575 <slamdunk7575@gmail.com>
2021-02-24 00:16:18 +0900

    (feat) Reward 구현 및 테스트

---
+++ src/main/java/ladder/reward/Reward.java
@@ -0,0 +1,26 @@
+package ladder.reward;
+
+import ladder.util.StringUtil;
+
+public class Reward {
+    private String name;
+
+    private Reward(String name) {
+        checkEmpty(name);
+        this.name = name;
+    }
+
+    public static Reward of(String name) {
+        return new Reward(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void checkEmpty(String name) {
+        if(StringUtil.isEmpty(name)) {
+            throw new IllegalArgumentException("당첨결과 이름이 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/ladder/reward/RewardTest.java
@@ -0,0 +1,33 @@
+package ladder.reward;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class RewardTest {
+
+    @Test
+    @DisplayName("당첨결과 생성되는지 확인")
+    public void reward_test() {
+        // given
+        String name = "1등";
+
+        // when
+        Reward reward = Reward.of(name);
+
+        // then
+        assertThat(reward.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("잘못된 상금을 입력한 경우 예외발생")
+    public void reward_input_error_test() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                   Reward.of(null);
+                   Reward.of("");
+                }).withMessageMatching("당첨결과 이름이 없습니다.");
+    }
+}
